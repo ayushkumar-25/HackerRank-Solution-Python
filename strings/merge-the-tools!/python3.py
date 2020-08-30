f1c6b04afc238a47a506613a287f24068831b718
ayushkumar25 <ayush2608@gmail.com>
2020-08-30 13:10:16 +0530

    Update python3.py

--- strings/merge-the-tools!/python3.py
+++ strings/merge-the-tools!/python3.py
@@ -1,3 +1,4 @@
+# Method-1 (Without using List Comprehension)
 def merge_the_tools(string, k):
     # your code goes here
     for i in range(0, len(string), k):
@@ -10,3 +11,14 @@ def merge_the_tools(string, k):
 if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
+
+
+# Method-2 (Using List Comprehension)
+def merge_the_tool(string, k):
+    for i in range(0, len(string), k):
+        u = []
+        print(''.join([j for j in string[i:i+k] if not (j in u or u.append(j))]))
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tool(string, k)
