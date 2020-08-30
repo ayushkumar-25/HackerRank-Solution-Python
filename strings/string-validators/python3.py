880d8a114035438389b2b6cb4dbe2fab852fac51
ayushkumar25 <ayush2608@gmail.com>
2020-08-30 13:42:39 +0530

    Update python3.py

--- strings/string-validators/python3.py
+++ strings/string-validators/python3.py
@@ -6,10 +6,12 @@ print(any (x.isdigit() for x in str))
 print(any (x.islower() for x in str))
 print(any (x.isupper() for x in str))
 
+
 # Method-2
 str = input()
 for test in ('isalnum', 'isalpha', 'isdigit', 'islower', 'isupper'):
     print(any(eval('x.' + test + '()') for x in str))
+
 
 # Method-3
 s = input()
